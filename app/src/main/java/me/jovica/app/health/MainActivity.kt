6080146eb9ca71d5422fe89c6ac4e3d19b4cef96
Jovica Veljković <60753340+vjxDev@users.noreply.github.com>
2024-06-29 00:37:16 +0200

    Start

---
+++ app/src/main/java/me/jovica/app/health/MainActivity.kt
@@ -0,0 +1,55 @@
+package me.jovica.app.health
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.jovica.app.health.ui.theme.JsHealthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JsHealthTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    LazyColumn( modifier = modifier) {
+        items(100) {
+            Text(
+                text = "Hello $name! $it",
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true, device = "id:pixel_7a")
+@Composable
+fun GreetingPreview() {
+    JsHealthTheme {
+        Greeting("Android")
+    }
+}
